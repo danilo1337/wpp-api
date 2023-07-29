2b0fec14681215d8f205b201c249c08c683f941b
danilo1337 <danilorrg@gmail.com>
2023-07-29 16:44:36 -0300

    * Adicionado Cors no recurso contatos * Alterado porta do sqlserver

--- src/main/java/br/com/danilo/controller/ContatoController.java
+++ src/main/java/br/com/danilo/controller/ContatoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,6 +18,8 @@ import br.com.danilo.exception.WppException;
 import br.com.danilo.service.ContatoService;
 import io.swagger.annotations.ApiOperation;
 
+
+@CrossOrigin("*")
 @RestController
 @RequestMapping("contatos")
 public class ContatoController {
